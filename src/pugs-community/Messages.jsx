b2bdfbc63ac747964e156770068afb5d61f09931
vladislavaSim <simonova.vladislava@gmail.com>
2021-09-09 18:08:21 +0300

    added messages component and profile component updated

---
+++ src/pugs-community/Messages.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Messages() {
+    return <>
+        <h3>You have not received any messages yet</h3>
+    </>
+}
--- src/pugs-community/Profile.jsx
+++ src/pugs-community/Profile.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {Link, useHistory} from "react-router-dom";
 import EditProfileInfo from "./EditProfileInfo";
 import {isEdit} from "./action/action";
+import {GetAPug} from "./GetAPug";
+import {Messages} from "./Messages";
 
 export function Profile() {
+
+    const [tab, setTab] = useState(1)
+
     const info = useSelector((state) => state.logInReducer);
     const isEditing = useSelector(state => state.toggleItemToEdit)
     const history = useHistory()
     const dispatch = useDispatch()
     function handleEditPage() {
         dispatch((isEdit()))
+        setTab(3)
     }
+
+    function switchTabs() {
+        if(tab === 1) {
+            return <GetAPug/>
+        } else if(tab === 2) {
+            return <Messages />
+        } else if(tab === 3){
+            return <EditProfileInfo />
+        } else {
+            return null
+        }
+    }
+
     function exit() {
         history.push('/main')
     }
-    const name = () => {
-        if(info.username) {}
-    }
-
+    console.log(switchTabs(2))
 
     return <div>
                 <div className='profile_container'>
@@ -28,10 +44,10 @@ export function Profile() {
                 <div className='buttons_container'>
                     <button onClick={() => handleEditPage()} className='user_box_buttons'>Edit profile</button>
                     <button className='user_box_buttons'>Search</button>
-                    <button className='user_box_buttons'>Messages</button>
-                    <button className='user_box_buttons'>Get a pug</button>
+                    <button className='user_box_buttons' onClick={() => setTab(2)}>Messages</button>
+                    <button className='user_box_buttons' onClick={() => setTab(1)}>Get a pug</button>
                     <button className='user_box_buttons' onClick={exit}>Exit</button>
                 </div>
-        <EditProfileInfo />
+        {switchTabs()}
         </div>
 }
